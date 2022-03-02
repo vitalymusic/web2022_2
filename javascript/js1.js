23d35f08db367f8984e7f2722b69e10095290fc4
Vitālijs KOrabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2022-03-02 20:17:44 +0200

    Added js1

--- javascript/js1.js
+++ javascript/js1.js
@@ -1,1 +1,38 @@
-alert("kuku");
+// Типы данных
+
+
+// Integer INT - Number
+let x = 123;
+x = "kuku";
+// String STR - Text
+let str = "Hello World";
+// Float
+let x2 = 2.45;
+//Boolean
+let bool = true; //false
+
+//Arrays
+let arr = ["BMW",2.5,true];
+
+// Objects
+let mobj1 = {
+    name: "Vitaly",
+    age:37
+}
+let 
+a = 1,
+b = 3,
+c = 4;
+let arr2;
+
+// document.write(x,str,bool); //вывод данных в документ
+// document.write(arr,obj1); //вывод данных в документ
+// document.write(arr[1]);
+// document.write(mobj1.age);
+
+document.write(arr3);
+
+// console.log(arr,obj1); //вывод данных в консоль
+
+//ES6 let
+const author = "V.K.";
