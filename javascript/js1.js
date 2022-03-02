8f9de62ecad74e81e6cf51fdec2f80a511fd07f0
Vitālijs KOrabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2022-03-02 20:56:20 +0200

    Added js1

--- javascript/js1.js
+++ javascript/js1.js
@@ -3,9 +3,10 @@
 
 // Integer INT - Number
 let x = 123;
-x = "kuku";
+//x = "kuku";
 // String STR - Text
 let str = "Hello World";
+let str2 = "Hello everyone!";
 // Float
 let x2 = 2.45;
 //Boolean
@@ -30,9 +31,48 @@ let arr2;
 // document.write(arr[1]);
 // document.write(mobj1.age);
 
-document.write(arr3);
+// document.write(arr3);
 
 // console.log(arr,obj1); //вывод данных в консоль
 
 //ES6 let
-const author = "V.K.";
+// const author = "V.K.";
+
+
+let answer = x * x2;
+ answer = x + x2;
+ answer = x - x2;
+ answer = x / x2;
+document.write(answer);
+
+// document.write(
+//         "<h1 class=\"title1\">" + str + "</h1>",
+//         "<h1 class=\"title2\">" + str2 + "</h1>" //Конкатенация \'
+//         );
+
+//ES6
+document.write(
+    `
+    <h1 class="title">${str}</h1>
+    <h1 class="title2">${str2}</h1>
+    `
+)
+
+
+// input from user
+
+// let ans = prompt("How old are you???");
+
+// alert("You are " + ans + " years old!");
+// let conf = confirm("Are you 18 years old?");
+// console.log(conf);
+
+let digit = +prompt("введите число");
+let digit2 = +prompt("введите второе число");
+
+document.write(digit * digit2);
+
+//сделать калькулятор отделочных работ (квадратура, высота потолка)
+// сделать кредитный калькулятор (Сумма займа, срок займа, ежемесячный платеж и переплата)
+// Зарплата после уплаты налогов
+//Отформаировать вывод (результат)
